7ac69b2fc507254e6a89097acf4fdb96e3a630f9
ErikMaidik914 <erik.maidik13@gmail.com>
2024-04-21 14:54:44 +0300

    DB implemented

--- MPPBackend/controllers/userController.js
+++ MPPBackend/controllers/userController.js
@@ -1,27 +1,34 @@
 import { User } from "../models/User.js";
 
-import { users } from "../dataStorage.js";
-
 //import mongoose from 'mongoose'
 
-export const getUsers = (req, res) => {
-  res.json(users);
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getUserById = async (req, res) => {
   const id = parseInt(req.params.id);
-  const user = users.find((user) => user.getId() == id);
-  if (user) res.status(200).json(user);
-  else res.status(404).json("User not found");
+  try {
+    const user = await User.findOne({ id: id });
+    if (user) res.status(200).json(user);
+    else res.status(404).json("User not found");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const createUser = async (req, res) => {
   try {
     console.log(req.body);
-    const { name, team, pictureUrl, age } = req.body;
+    const { name, team, url, age } = req.body;
 
-    const user = new User(name, team, pictureUrl, age);
-    users.push(user);
+    const user = new User({ name, team, url, age });
+    await user.save();
     return res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -30,29 +37,28 @@ export const createUser = async (req, res) => {
 
 export const deleteUser = async (req, res) => {
   const id = parseInt(req.params.id);
-  const index = users.findIndex((user) => user.getId() == id);
-  if (index > -1) {
-    users.splice(index, 1);
-    res.status(200).json("User deleted");
-  } else {
-    res.status(400).json("User mot found");
+  try {
+    const user = await User.findOneAndDelete({ id: id });
+    if (user) res.status(200).json("User deleted");
+    else res.status(404).json("User not found");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
 export const updateUser = async (req, res) => {
   const id = parseInt(req.params.id);
-
-  const user = users.find((user) => user.getId() == id);
-
   try {
-    const { name, team, pictureUrl, age } = req.body;
-    user.setName(name);
-    user.setTeam(team);
-    user.setPictureUrl(pictureUrl);
-    user.setAge(age);
-    return res.status(200).json(user);
+    const { name, team, url, age } = req.body;
+    const user = await User.findOneAndUpdate(
+      { id: id },
+      { name, team, url, age },
+      { new: true }
+    );
+    if (user) res.status(200).json(user);
+    else res.status(404).json("User not found");
   } catch (error) {
-    res.status(400).json("user not found");
+    res.status(400).json("User not found");
   }
 };
 
--- MPPBackend/models/UserSchema.js
+++ MPPBackend/models/UserSchema.js
@@ -16,6 +16,9 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    url: {
+      type: String,
+    },
     age: {
       type: Number,
       required: true,
